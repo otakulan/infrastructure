5ec6b3147ffdd8222b19b10acd9ff7531d822959
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-05-25 23:34:54 -0400

    snmp-exporter: update default config

--- monitoring/snmp.nix
+++ monitoring/snmp.nix
@@ -3,8 +3,8 @@
 with lib;
 let
   defaultSnmpConfig = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/prometheus/snmp_exporter/969516950f086a1d86968d7a89b976a092c8191f/snmp.yml";
-    sha256 = "sha256-9YPeINfamINpZKtx+dcJc99rBe9cJW0l8SrIM3GYlZ4=";
+    url = "https://raw.githubusercontent.com/prometheus/snmp_exporter/44f8732988e726bad3f13d5779f1da7705178642/snmp.yml";
+    sha256 = "sha256-WXlpIIurEBnhAQ2kE2vXq28i+0N7v4jHuD/L3NQU+AY=";
   };
 in {
   services.prometheus.exporters.snmp = {
