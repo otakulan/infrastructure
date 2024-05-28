27863344b31e5a0f626a8d144a1982b75cb946d4
Tristan Gosselin-Hane <starcraft66@gmail.com>
2024-05-27 20:17:26 -0400

    lanparty-seating: make it run and add access restrictions

--- lanparty-seating/lanparty-seating.nix
+++ lanparty-seating/lanparty-seating.nix
@@ -2,7 +2,32 @@
 
 {
   # Secrets in here will be encrypted next year
-  networking.firewall.interfaces.eth0.allowedTCPPorts = [ 4000 ];
+  networking.firewall.interfaces.eth0.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.trustedInterfaces = [ "podman0" ];
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "default" = {
+        default = true;
+        locations."/" = {
+          # restrict admin interface to admin VLAN
+          extraConfig = ''
+            allow 172.16.2.0/24;
+            deny all;
+          '';
+          proxyPass = "http://127.0.0.1:4000/";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+        locations."/desktop" = {
+          # Desktop client socket wide open
+          proxyPass = "http://127.0.0.1:4000/desktop";
+          proxyWebsockets = true;
+          recommendedProxySettings = true;
+        };
+      };
+    };
+  };
   virtualisation.oci-containers = {
     backend = "podman";
     containers = {
@@ -20,10 +45,12 @@
         ];
       };
       lanparty-seating = {
-        image = "ghcr.io/starcraft66/lanparty-seating/lanparty-seating:1.0.0";
+        image = "ghcr.io/otakulan/lanparty-seating/lanparty-seating:1.0.0";
         autoStart = true;
-        ports = [ "${config.env.staticIpv4}:4000:4000/tcp" ];
+        ports = [ "127.0.0.1:4000:4000/tcp" ];
         environment = {
+          PHX_HOST = "172.16.2.7";
+          MIX_ENV = "prod";
           SECRET_KEY_BASE = "nah5choh8fohnoap1ien3OoKeehei2ch";
           DATABASE_URL = "ecto://postgres:postgres@172.16.2.7/lanpartyseating_prod";
           TZ = "America/Toronto";
